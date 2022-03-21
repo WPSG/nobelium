8b6684ea6454d8fa588d02797dfbc130e4305235
Roc <roc.w@foxmail.com>
2022-03-21 16:23:57 +0800

    Modify blog.config.js & Delete *.svg

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: process.env.TITLE,
-  author: process.env.AUTHOR,
+  title: 'PENSIEVE',
+  author: 'ROC',
   email: process.env.EMAIL,
-  link: process.env.LINK,
-  description: process.env.DESCRIPTION,
+  link: 'https://www.rocplus.tk',
+  description: '认识自己|探索世界|好好生活',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -15,9 +15,9 @@ const BLOG = {
   sortByDate: true,
   showAbout: true,
   showArchive: true,
-  autoCollapsedNavBar: true, // The automatically collapsed navigation bar
-  ogImageGenerateURL: process.env.OG_URL, // The link to generate OG image, don't end with a slash
-  socialLink: process.env.SOCIALLINK,
+  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  socialLink: 'https://www.rocplus.tk',
   seo: {
     keywords: ['ROC', 'Pensieve', 'Blog', 'Notion-Blog', 'Website', 'Notion', 'Nobelium'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
@@ -25,11 +25,11 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: process.env.ANALYTICS_PROVIDER, // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ackee', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
-      tracker: process.env.ACKEE_TRACKER, // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: process.env.DATA_ACKEE_SERVER, // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: process.env.ACKEE_DOMAIN_ID // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      tracker: 'https://ackee-roc.vercel.app/tracker.js', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: 'https://ackee-roc.vercel.app', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: '487d2c26-7696-4fa9-b013-a06a0bb4353c' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
       measurementId: '' // e.g: G-XXXXXXXXXX
@@ -37,7 +37,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: process.env.COMMENT_PROVIDER, // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -50,9 +50,9 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: process.env.CUSDIS_APPID, // data-app-id
-      host: process.env.CUSDIS_HOST, // data-host, change this if you're using self-hosted version
-      scriptSrc: process.env.CUSDIS_SCRIPTSRC // change this if you're using self-hosted version
+      appId: '487d2c26-7696-4fa9-b013-a06a0bb4353c', // data-app-id
+      host: 'https://cusdis-roc.vercel.app', // data-host, change this if you're using self-hosted version
+      scriptSrc: 'https://cusdis-roc.vercel.app/js/cusdis.es.js' // change this if you're using self-hosted version
     }
   },
   isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
